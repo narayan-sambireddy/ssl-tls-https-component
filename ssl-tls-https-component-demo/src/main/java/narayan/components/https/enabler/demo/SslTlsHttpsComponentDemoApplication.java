e040319d71e966261b0aa91ea20453dbb036f715
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-07-12 23:56:29 +0530

    Initial Commit

---
+++ ssl-tls-https-component-demo/src/main/java/narayan/components/https/enabler/demo/SslTlsHttpsComponentDemoApplication.java
@@ -0,0 +1,12 @@
+package narayan.components.https.enabler.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslTlsHttpsComponentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslTlsHttpsComponentDemoApplication.class, args);
+	}
+}
