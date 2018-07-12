b5175f528def6efc266c327b5d399a442570ee38
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-07-12 23:53:45 +0530

    Initial Commit

---
+++ ssl-enable-tls/src/main/java/narayan/components/https/enabler/SslEnableTlsApplication.java
@@ -0,0 +1,12 @@
+package narayan.components.https.enabler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SslEnableTlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SslEnableTlsApplication.class, args);
+	}
+}
